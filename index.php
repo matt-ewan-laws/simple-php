492248942fbdc8bbd98684f48487a719a8534c95
Matthew Ewan-Laws <matt@giantpeach.agency>
2022-08-09 22:13:41 +0100

    add tailwind

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang=en>
+    <head>
+        <title>Projects</title>
+        <link rel="stylesheet" href="/style.css">
+    </head>
+
+    <body>
+
+        <p class="text-red-600">Hello world</p>
+    </body>
+</html>
---
+++ watch.sh
@@ -0,0 +1,1 @@
+./tailwindcss -i src.css -o style.css --watch
