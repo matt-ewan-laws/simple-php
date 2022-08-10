97c1397c440aae1917208ef1e0edb2dca8749212
Matthew Ewan-Laws <matt@giantpeach.agency>
2022-08-10 08:55:40 +0100

    add routing

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!doctype html>
-<html lang=en>
-    <head>
-        <title>Projects</title>
-        <link rel="stylesheet" href="/style.css">
-    </head>
-
-    <body>
-
-        <p class="text-red-600">Hello world</p>
-    </body>
-</html>
+<?php
+require 'lib/routing.php';
+require 'routes.php';
